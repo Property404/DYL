225c4cd3353d736802d9836dcd7c2ec69cf7665f
Bethimus <pyrokid5505@yahoo.com>
2016-03-13 04:54:00 -0400

    Update insulter.py

--- insulter.py
+++ insulter.py
@@ -1,9 +1,9 @@
 import random
 def insult():
 	prefixes = ["pig-slapping","cum-slurping","jimmy-jostling","anime-loving","garbage-eating",
-	"puppy-fucking","small-cocked"]
+	"puppy-fucking"]
 	
-	nouns = ["small-cocked","cock monkey","grass eater","cod piece","buritto fucker","youtube commenter",
+	nouns = ["cock monkey","grass eater","cod piece","buritto fucker","youtube commenter",
 	"child licker","cake-sniffer"]
 	
 	return random.choice(prefixes)+" "+random.choice(nouns)
