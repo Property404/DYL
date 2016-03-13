d6ad69f88a139286083db13df51a17ff6738d6b3
Dagan Gan <daganboy@gmail.com>
2016-03-13 05:06:35 -0400

    Fixed insults

--- insulter.py
+++ insulter.py
@@ -1,16 +1,9 @@
 import random
 def insult():
 	prefixes = ["pig-slapping","cum-slurping","jimmy-jostling","anime-loving","garbage-eating",
-<<<<<<< HEAD
-	"puppy-fucking","small-cocked","creepy old","pedophilic","mountain-dew-drinking and dorito-eating","fedora-wearing"]
-	
-	nouns = ["small-cocked","cock monkey","grass eater","cod piece","buritto fucker","youtube commenter",
-	"child licker","cake-sniffer","neckbeard","asscrack whore"]
-=======
-	"puppy-fucking"]
+	"puppy-fucking","creepy old","pedophilic","mountain-dew-drinking, dorito-eating","fedora-wearing"]
 	
 	nouns = ["cock monkey","grass eater","cod piece","buritto fucker","youtube commenter",
-	"child licker","cake-sniffer"]
->>>>>>> 225c4cd3353d736802d9836dcd7c2ec69cf7665f
-	
+	"child licker","cake-sniffer","neckbeard","asscrack whore"]
+
 	return random.choice(prefixes)+" "+random.choice(nouns)
