15feab9ca3d0641e5bebe0e3308737db7263ec9b
Bethimus <pyrokid5505@yahoo.com>
2016-03-12 19:38:43 -0500

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -1,10 +1,10 @@
 import tweepy
-auth = tweepy.OAuthHandler("aYFy76da9sIiKE1fvPHDk0g04", "WEJRcjRSXXYBLjPv8Yd1Jl52ad2LAcORHew9sEI8DpRUJ9QiN1")
-auth.set_access_token("3173400849-YzK5LEyUafJCbekegCSPANyoBqfdgOVvRUTTQQI", "kwHh7w2Q35dOdbsJKqYBOOyyWjmhdImXuRSVTHBxmYfab")
+auth = tweepy.OAuthHandler("oR5SCZYrzk1OzK8f6hc6LRF9w", "9pAnmPEG27CyYjTZV9X1pxflc04CtBq8LliHBIchOQJzW0XnHo")
+auth.set_access_token("708791448796340224-UgKVZqkpjhqJJD0JKLaP1gBu72VklrA", "Go0Zd5jajbVwP2M2jCEmMfhmGvDYroWnAGlHjJoCnotFw")
 api = tweepy.API(auth)
 def make_tweet(twitter_id,account=api,full_name=None,msg="{0} just ruined their entire life!",hashtag=[]):
     try:
-	text=msg.format('@'+twitter_id)+"("+str(random.randint(0,1000))+") "
+	text=msg.format('@'+twitter_id)
 	for i in hashtag:
 		text+="#"+i+" "
         account.update_status(status=text)
